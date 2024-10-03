03390c4a9017b4681e73c6dc94d04fd7f5d3f38c
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-10-04 02:01:22 +0500

    menu is fully responsive and working as per requirenent

--- src/app/components/navBar.tsx
+++ src/app/components/navBar.tsx
@@ -1,22 +1,51 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import dataCrunchLogo from "@/assets/dataCrunchIO.svg";
 import Image from "next/image";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  // Toggle menu on button click
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+    console.log("Button Clicked");
+  };
+
+  // Close the menu when clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        //@ts-ignore
+        !buttonRef?.current.contains(event.target) // This stops closing when the button is clicked
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    // Attach event listener for outside click
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="sticky top-0 z-50 bg-white w-full border-b border-gray-300">
-      <nav className=" container mx-auto flex justify-between items-baseline- px-4 md:px-8 py-2 md:py-4 ">
+      <nav className=" container mx-auto flex justify-between items-center px-4 md:px-8 py-2 md:py-4 ">
         <Image
           src={dataCrunchLogo}
           alt="DataCrunch.io Logo"
-          className="w-40 md:w-60 object-contain cursor-pointer"
+          className="w-40 md:w-60 object-contain cursor-pointer lg:pb-2"
         />
 
-        <div className="hidden lg:flex space-x-6 text-[#707070] font-medium items-end  text-lg ">
+        <div className="hidden lg:flex space-x-6 text-[#707070] font-medium items-baseline text-lg ">
           <Link href="#" className="hover:underline">
             GPU Cloud
           </Link>
@@ -37,8 +66,8 @@ export default function NavBar() {
           </Link>
         </div>
 
-        <div className=" flex justify-center space-x-4 font-semibold text-[#707070] max-lg:hidden lg:block">
-          <Link href="#" className=" hover:text-gray-700">
+        <div className=" flex justify-center items-center space-x-4 font-semibold text-[#707070] max-lg:hidden lg:block">
+          <Link href="#" className=" hover:text-gray-700 px-4 py-2">
             Login
           </Link>
           <Link
@@ -49,18 +78,19 @@ export default function NavBar() {
           </Link>
         </div>
         <button
-          onClick={() => setIsOpen(!isOpen)}
+          ref={buttonRef}
+          onClick={toggleMenu}
           className="text-3xl lg:hidden focus:outline-none z-60"
         >
           <IoMdMenu />
         </button>
-        {/* Mobile Dropdown Menu */}
       </nav>
-      {/* {isOpen && ( */}
+      {/* Mobile Dropdown Menu */}
       <div
-        className={`lg:hidden overflow-hidden  transition-all duration-500 ease-in-out ${
-          isOpen ? "max-h-screen" : "max-h-0"
-        }`}
+        ref={menuRef}
+        className={`lg:hidden overflow-hidden  transition-all duration-500 ease-in-out
+            ${isOpen ? "max-h-screen" : "max-h-0"}
+          `}
       >
         <div className="flex flex-col space-y-2 container mx-auto justify-center md:justify-start font-medium text-[#707070] py-2 px-4 md:px-8">
           <Link href="#" className="hover:underline">
@@ -95,7 +125,6 @@ export default function NavBar() {
           </div>
         </div>
       </div>
-      {/* )} */}
     </div>
   );
 }
